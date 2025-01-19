923e713075b70c600642e06b159ff0284977de23
ZeroWillHero <ict21018@std.uwu.ac.lk>
2025-01-20 05:08:03 +0530

    change auth state (Hard Coded)

--- src/pages/auth/ProtectedRoutes.jsx
+++ src/pages/auth/ProtectedRoutes.jsx
@@ -2,7 +2,7 @@ import { Navigate } from "react-router-dom";
 import PropTypes from 'prop-types';
 
 const ProtectedRoutes = ({ children }) => {
-    const isAuth = true; // This should be replaced with your actual auth check logic
+    const isAuth = false; // This should be replaced with your actual auth check logic
     
     if (!isAuth) {
         return <Navigate to="/login" replace />; // Use replace to avoid adding the login route to history
