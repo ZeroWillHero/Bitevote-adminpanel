95f7892520cbf7f94ddf3da815ca4673c2aa7542
ZeroWillHero <ict21018@std.uwu.ac.lk>
2025-01-20 02:42:50 +0530

    Added Firebase Init

---
+++ src/firebase.init.js
@@ -0,0 +1,16 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBdF7FCufW4q2qZbs8py43raTV93C2aLVM",
+  authDomain: "bitevote-a7a25.firebaseapp.com",
+  projectId: "bitevote-a7a25",
+  storageBucket: "bitevote-a7a25.firebasestorage.app",
+  messagingSenderId: "544181393584",
+  appId: "1:544181393584:web:20b0baf3d9fe539e6fc375"
+};
+
+export const app = initializeApp(firebaseConfig);
