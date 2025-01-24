fad4453e49d489283eae252cddcbffe10e632005
ZeroWillHero <ict21018@std.uwu.ac.lk>
2025-01-24 15:10:22 +0530

    Login and Registers are intergrated

--- src/pages/auth/Login.jsx
+++ src/pages/auth/Login.jsx
@@ -1,16 +1,54 @@
 import { assets } from "../../assets/assets";
+import { Link } from "react-router-dom";
 import './auth.css';
+import { useState } from "react";
+import { loginWithEmailAndPassword } from "../../auth/authentication";
+import { useNavigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { setAccessToken, setEmail, setUid } from "../../global/authentication/createSlice";
+
 export default function Login() {
+  const [data,setData] = useState();
+  const [isLoading,setIsLoading] = useState(false);
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const handleData = (event) => {
+
+    setData({
+      ...data,
+      [event.target.name]: event.target.value,
+
+    });
+  }
+
+  
+
+  const submitData = async (e) => {
+    e.preventDefault()
+    setIsLoading(true)
+    const response = await loginWithEmailAndPassword(data.email,data.password);
+    dispatch(setUid(response.user.uid));
+    dispatch(setAccessToken(response.accessToken));
+    dispatch(setEmail(response.user.email));
+    // set localStorage Values 
+    localStorage.setItem("accessToken" ,response.accessToken);
+    setIsLoading(false);
+    navigate("/");
+  }
   return (
     <div className="main-wrapper">
        <div className="auth-wrapper">
         <h2 className="heading">Shop Keeper Portal</h2>
           <img src={assets.logo} alt="logo" />
-          <form>
-            <input type="text" placeholder="email" className="input-decoration"/>
-            <input type="password" placeholder="password" className="input-decoration"/>
-            <button className="btn">Login</button>
+          <form onSubmit={submitData}>
+            <input type="text" placeholder="email" className="input-decoration" name="email" onChange={handleData}/>
+            <input type="password" placeholder="password" className="input-decoration" name="password" onChange={handleData}/>
+            <button className="btn">{isLoading ? "Log in processing" : "Login"}</button>
           </form>
+
+          <p>Do not Have an Account? <Link to='/register'>Register</Link></p>
+
        </div>
     </div>
   );
