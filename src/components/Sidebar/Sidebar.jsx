30da825c7f95c8f504a860cd476959c2bc97fc10
ZeroWillHero <ict21018@std.uwu.ac.lk>
2025-01-24 15:09:14 +0530

    Added Sidebar

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,24 +1,33 @@
-import React from 'react';
 import './Sidebar.css';
 import { assets } from '../../assets/assets';
-import { NavLink } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { setPage } from '../../global/sideBar/createSlice';
 
 const Sidebar = () => {
+  const dispatch = useDispatch();
+
+  const handleChange = (page) => () => {
+    dispatch(setPage(page));
+  // console.log(page); 
+
+  }
+
+
   return (
     <div className="sidebar">
       <div className="sidebar-options">
-        <NavLink to="/add" className="sidebar-option">
-          <img src={assets.add_icon} alt="" />
+        <button onClick={handleChange("add")} className="sidebar-option">
+          <img src={assets.add_icon} />
           <p>Add Items</p>
-        </NavLink>
-        <NavLink to="/list" className="sidebar-option">
-          <img src={assets.order_icon} alt="" />
+        </button>
+        <button onClick={handleChange("list")} className="sidebar-option">
+          <img src={assets.order_icon}/>
           <p>List Items</p>
-        </NavLink>
-        <NavLink to="/orders" className="sidebar-option">
-          <img src={assets.order_icon} alt="" />
+        </button>
+        <button onClick={handleChange("orders")} className="sidebar-option">
+          <img src={assets.order_icon} />
           <p>Orders</p>
-        </NavLink>
+        </button>
       </div>
     </div>
   );
