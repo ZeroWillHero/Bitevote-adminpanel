22c029ac2f87feff57d6a18426eb7e8ec8100e8d
ZeroWillHero <ict21018@std.uwu.ac.lk>
2025-01-24 15:09:04 +0530

    updated Firebase Login and register Routes

--- src/auth/authentication.jsx
+++ src/auth/authentication.jsx
@@ -1,27 +1,35 @@
-import { getAuth } from "firebase/auth";
-import { app } from "../firebase.init";
 import { toast } from "react-toastify";
+import { auth } from "../firebase.init";
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
+import { getFirestore, doc, setDoc } from "firebase/firestore"; 
 
-const auth = getAuth(app);
 
-export const registerWithEmailAndPassword = async ( email, password ) => {
-    auth.CreateUserWithEmailAndPassword(auth,email, password)
-    .then(() => {
-        console.log('User created');
-        toast.success('User created');
-    }).catch((error) => {
-        console.error(error);
-        toast.error(error.message);
-    });
+
+const db = getFirestore();
+export const register = async (email, password,user) => {
+  try{
+    const userCredential = await createUserWithEmailAndPassword(auth, email, password);
+    // add FireStore 
+    const docRef = doc(db, "users", userCredential.user.uid);
+    await setDoc(docRef, user);
+    toast.success("User registered successfully");
+  }catch(error){
+    console.log(error);
+    toast.error(error.message);
+  }
 };
 
-export const loginWithEmailAndPassword = async (auth, email, password ) => {
-    auth.signInWithEmailAndPassword(email, password)
-    .then(() => {
-        console.log('User logged in');
-        toast.success('User logged in');
-    }).catch((error) => {
-        console.error(error);
-        toast.error(error.message);
-    });
+export const loginWithEmailAndPassword = async (email, password) => {
+
+  try {
+    const response = await signInWithEmailAndPassword(auth, email, password);
+    console.log(response);
+    toast.success("User logged in successfully");
+    return response;
+
+  } catch (error) {
+    toast.error(error.message);
+  }
+
+
 }
