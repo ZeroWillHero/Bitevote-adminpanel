d30b09930940008e484d4daa65b68022e7b2e06e
ZeroWillHero <ict21018@std.uwu.ac.lk>
2025-01-24 15:08:22 +0530

    layout created

--- src/layout.jsx
+++ src/layout.jsx
@@ -1,14 +1,27 @@
 import Navbar from "./components/Navbar/Navbar"
 import Sidebar from "./components/Sidebar/Sidebar"
+import Add from "./pages/Add/Add"
+import List from "./pages/List/List"
+import Orders from "./pages/Orders/Orders"
+
+import { useSelector } from 'react-redux';
+
 
 
 export default function Layout() {
+    const url = "http://localhost:4000";
+    const page = useSelector((state) => state.sideBar.page);
+    console.log("From main Wrapper",page);
     return (
         <div>
             <Navbar />
             <hr />
             <div className="app-content">
             <Sidebar />
+            {
+                page === "add" ? <Add url={url} /> : page === "list" ? <List url={url} /> : <Orders url={url}/>
+            }
+            
         </div>
         </div>
         
