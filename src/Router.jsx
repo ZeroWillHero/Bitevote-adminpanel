1aa95083f6e89d707852c04c4e3a1da0cc27d10a
ZeroWillHero <ict21018@std.uwu.ac.lk>
2025-01-24 15:08:44 +0530

    Change Routes wit h protected Routes

--- src/Router.jsx
+++ src/Router.jsx
@@ -10,7 +10,7 @@ export default function Router() {
         <Routes>
             <Route path='/login' element={<Login />} />
             <Route path='/register' element={<Register />} />
-            <Route path='/dashboard' element={
+            <Route path='/' element={
                 <ProtectedRoutes>
                     <Layout />
                 </ProtectedRoutes>
